6a61b545237bf54b1652a005c98257106540f856
timothykozak <timothykozak@yahoo.com>
2019-09-05 15:32:41 -0400

    Does basic validation of the new grave.

--- src/PBGrave.ts
+++ src/PBGrave.ts
@@ -4,24 +4,33 @@
 
 import {LatLngLit, SerializableGrave} from "./PBInterfaces.js";
 
+const DEFAULT_OFFSET: LatLngLit = {lat: 0, lng: 0};
+const DEFAULT_SIZE: LatLngLit = {lat: 0, lng: 0};
+
 class PBGrave implements SerializableGrave {
+
 
     offset: LatLngLit;
     angle: number;
     size: LatLngLit;
     name: string;
     dates: string;
+    validGrave: boolean;
+
 
     constructor(public map: google.maps.Map, theSG: SerializableGrave) {
         this.deSerialize(theSG);
     }
 
     deSerialize(theSG: SerializableGrave) {
-        this.offset = theSG.offset;
-        this.angle = theSG.angle;
-        this.size = theSG.size;
-        this.name = theSG.name;
-        this.dates = theSG.dates;
+        this.validGrave = true;
+        this.offset = (theSG.offset !== null) ? theSG.offset : DEFAULT_OFFSET;
+        this.angle = (theSG.angle !== null)  ? theSG.angle : 0;
+        this.size = (theSG.size !== null)  ? theSG.size : DEFAULT_SIZE;
+        this.name = (theSG.name !== null)  ? theSG.name : '';
+        this.dates = (theSG.dates !== null)  ? theSG.dates : '';
+        if ((this.name.length + this.dates.length) == 0)
+            this.validGrave = false;
     }
 
     serialize(): string {
