8690569d29425fbf297103e77327bc7e8ab0481c
Tim <timothykozak@yahoo.com>
2019-09-11 08:43:06 -0400

    Now supports import individual cemeteries.

--- src/PBUIPanel.ts
+++ src/PBUIPanel.ts
@@ -1,17 +1,21 @@
 // PBUIPanel.ts
 //
-//
+//  This class is the user interface to the app.
+//  Currently it only supports the importing of grave data to a cemetery.
 
-import {LatLngLit, SerializableGrave} from "./PBInterfaces.js";
+import {SerializableGrave} from "./PBInterfaces.js";
 import {PBGrave} from "./PBGrave.js";
 import {PBCemetery} from "./PBCemetery.js";
 
 class PBUIPanel {
     uiPanel: HTMLDivElement;
+    selectElement: HTMLSelectElement;
 
     constructor(public map: google.maps.Map, public cemeteries: Array<PBCemetery>) {
         this.uiPanel = document.getElementById("uipanel") as HTMLDivElement;
         this.uiPanel.innerHTML = this.buildUIPanelHTML();
+        this.selectElement = document.getElementById("cemeteryselect") as HTMLSelectElement;
+        this.selectElement.selectedIndex = 0;
     }
 
     buildUIPanelHTML(): string {
@@ -30,7 +34,11 @@ class PBUIPanel {
     }
 
     importGraves() {
-        let theCemetery: PBCemetery = this.cemeteries[0];
+        // Only supports a very simple import of name and date pairs.
+        // The first line of the pair is a \n terminated string with the full name.
+        // The second line is a \n terminated string with some type of date.
+        // The date is only stored as a string and is not validated in any way.
+        let theCemetery: PBCemetery = this.cemeteries[this.selectElement.selectedIndex];
         let textElement: HTMLInputElement= document.getElementById("importtext") as HTMLInputElement;
         let textToImport: string = textElement.value;
         textToImport += "\n";   // Just in case
