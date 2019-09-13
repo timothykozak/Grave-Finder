0add9e7f3e6835778534376e6b10c2c0ab2465b8
Tim <timothykozak@yahoo.com>
2019-09-13 15:15:26 -0400

    Removed rotation methods.

--- saveJSON.php
+++ saveJSON.php
@@ -7,23 +7,25 @@ function last_error_message() {
     return($last_err['message']);
 }
 
-$result = "";
+$message = "";
+$success = false;
 $file_name = ".\assets\cemeteries.txt";
 $content = trim(file_get_contents("php://input"));    //Receive the RAW post data.
 if (error_get_last()) {
-    $result = "Could not access content: " . last_error_message();
+    $message = "Could not access content: " . last_error_message();
 } else {
     $decoded = json_decode($content, true);
     if (is_array($decoded)) {
         $file_size = file_put_contents($file_name, $content);    // Update contents.  Create file if necessary
         if ($file_size) {
-            $result = "Save successful.";
+            $success = true;
+            $message = "File saved.";
         } else {
-            $result = "Could not save.  " . last_error_message();
+            $message = "Could not save: " . last_error_message();
         }
     } else {
-        $result = "Invalid JSON.  Not saved." . last_error_message();
+        $message = "Invalid JSON: " . last_error_message();
     }
 }
-echo $result;
+echo '{ "success": ' . $success . ', "message": "' . $message . '" }';
 ?>
