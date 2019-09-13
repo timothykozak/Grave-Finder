8fd311a5ff1d06a5c16714ea5bd9dbd76657f323
Tim <timothykozak@yahoo.com>
2019-09-13 13:24:55 -0400

    Added some basic validation.

--- saveJSON.php
+++ saveJSON.php
@@ -1,8 +1,29 @@
 <?php
 // Accepts the JSON and saves it as cemeteries.txt.
+// Does some basic validation and returns a result.
 
-    $file_name = ".\assets\cemeteries.txt";
-    $content = file_get_contents("php://input");    //Receive the RAW post data.
-    file_put_contents($file_name, $content);
+function last_error_message() {
+    $last_err = error_get_last();
+    return($last_err['message']);
+}
 
+$result = "";
+$file_name = ".\assets\cemeteries.txt";
+$content = trim(file_get_contents("php://input"));    //Receive the RAW post data.
+if (error_get_last()) {
+    $result = "Could not access content: " . last_error_message();
+} else {
+    $decoded = json_decode($content, true);
+    if (is_array($decoded)) {
+        $file_size = file_put_contents($file_name, $content);    // Update contents.  Create file if necessary
+        if ($file_size) {
+            $result = "Save successful.";
+        } else {
+            $result = "Could not save.  " . last_error_message();
+        }
+    } else {
+        $result = "Invalid JSON.  Not saved." . last_error_message();
+    }
+}
+echo $result;
 ?>
