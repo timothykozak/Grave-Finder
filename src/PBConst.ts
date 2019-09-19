94487090ec7808c7ba0ad9b21fa89c4098c4e783
Tim <timothykozak@yahoo.com>
2019-09-19 14:44:24 -0400

    Added some events.

--- src/PBConst.ts
+++ src/PBConst.ts
@@ -6,11 +6,13 @@
 
 class PBConst {
     static EVENTS = {
-        postJSON: "PBGFPostJSON",                   // event.detail = undefined
-        postJSONResponse: "PBGFPostJSONResponse",   // event.detail = {success: boolean, message: string}
-        importGraves: "PBGFImportGraves",           // event.detail = undefined
+        postJSON: 'PBGFPostJSON',                   // event.detail = undefined
+        postJSONResponse: 'PBGFPostJSONResponse',   // event.detail = {success: boolean, message: string}
+        importGraves: 'PBGFImportGraves',           // event.detail = undefined
+        requestPassword: 'PBGFRequestPassword',     // event.detail = undefined
+        closeEditControls: 'PBGFCloseEditControls', // event.detail = undefined
 
-        unload: "unload"
+        unload: 'unload'
     }
 }
 
