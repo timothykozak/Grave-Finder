1548fd5bf03f5b875628525fee22677374c5b290
Tim <timothykozak@yahoo.com>
2019-09-25 06:01:19 -0400

    Basic row select/deselect.

--- src/PBGraveSearch.ts
+++ src/PBGraveSearch.ts
@@ -11,17 +11,27 @@ import {PBConst} from './PBConst.js';
 class PBGraveSearch {
     tableElement: HTMLTableElement;
     tableBodyElement: HTMLTableSectionElement;
+    tableOverlayElement: HTMLDivElement;
+
     graveStrings: Array<string>;
+    private _editing: boolean;
 
     constructor(public map: google.maps.Map, public cemeteries: Array<PBCemetery>) {
         this.buildTable();
+        this.buildTableOverlay();
         this.populateTable(-1);
+        this.initEventListeners();
+    }
+
+    initEventListeners () {
         window.addEventListener(PBConst.EVENTS.selectGraveRow, (event: CustomEvent) => {this.onSelectGraveRow(event);});
+        window.addEventListener(PBConst.EVENTS.unselectGraveRow, (event: Event) => {this.onUnselectGraveRow(event);});
     }
 
     buildTable() {
         this.tableElement = document.createElement('table');
         this.tableElement.className = 'fixed-header-scrollable-table';
+        this.tableElement.contentEditable = 'false';
         this.tableElement.innerHTML = `<thead>\n
                     <tr>\n<th>Cemetery</th>\n<th>Name</th>\n<th>Dates</th>\n<th>Location</th>\n</tr>\n
                   </thead>`;
@@ -29,7 +39,25 @@ class PBGraveSearch {
         this.tableElement.appendChild(this.tableBodyElement);
     }
 
+    buildTableOverlay(){
+        this.tableOverlayElement = document.createElement('div');
+        this.tableElement.appendChild(this.tableOverlayElement);
+        this.tableOverlayElement.className = 'table-overlay';
+        this.tableOverlayElement.innerHTML = `<button>Previous</button>
+                                              <button>Next</button>
+                                              <button onclick="window.dispatchEvent(new Event('${PBConst.EVENTS.unselectGraveRow}'))" >Close</button>`;
+    }
 
+    set editing(theValue: boolean) {
+        this._editing = theValue;
+        if (!theValue) {
+            this.closeOverlay();
+        }
+    }
+
+    get editing(): boolean {
+        return(this._editing);
+    }
 
     onInput(theText: string) {
         theText.toLowerCase();
@@ -47,7 +75,17 @@ class PBGraveSearch {
     }
 
     onSelectGraveRow(event: CustomEvent) {
+        if (this._editing){
+            this.tableOverlayElement.style.display = 'block';
+        }
+    }
 
+    closeOverlay() {
+        this.tableOverlayElement.style.display = 'none';
+    }
+
+    onUnselectGraveRow(event: Event) {
+        this.closeOverlay();
     }
 
     populateTable(theCemetery: number) {
@@ -63,6 +101,7 @@ class PBGraveSearch {
         for (let index = startIndex; index <= endIndex; index++) {
             this.cemeteries[index].graves.forEach((grave: PBGrave) => {
                 theHTML += `<tr class="${(graveIndex % 2) ? 'odd-row' : 'even-row'}"
+                                contenteditable="false"
                                 onclick="window.dispatchEvent(new CustomEvent('${PBConst.EVENTS.selectGraveRow}', { detail:{ index: ${graveIndex}}} ));">
                                 <td>${this.cemeteries[index].name}</td><td>${grave.name}</td><td>${grave.dates}</td><td>unknown</td>
                             </tr>`;
