98ad4ec0c81bb89414a78cd846bd0248fde4130b
Tim <timothykozak@yahoo.com>
2019-09-30 09:01:27 -0400

    Now de/serializes the graves.

--- src/PBPlot.ts
+++ src/PBPlot.ts
@@ -4,7 +4,6 @@
 // with one or more places for a grave.
 
 import {PBGrave} from './PBGrave.js';
-import {PBCemetery} from './PBCemetery.js';
 import {LatLngLit, SerializablePlot} from './PBInterfaces';
 import {PBConst} from './PBConst.js';
 
@@ -18,17 +17,22 @@ class PBPlot implements SerializablePlot {
     location: LatLngLit;
     angle: number;
     numGraves: number;
+    graves: Array<PBGrave>;
 
     constructor(public map: google.maps.Map, theSP: SerializablePlot) {
         this.deSerialize(theSP);
     }
-
 
     deSerialize(theSP: SerializablePlot) {
         this.id = !(theSP.id == null) ? theSP.id : DEFAULT_ID;
         this.location = !(theSP.location == null) ? theSP.location : DEFAULT_LATLNGLIT;
         this.angle = !(theSP.angle == null)  ? theSP.angle : DEFAULT_ANGLE;
         this.numGraves = !(theSP.numGraves == null)  ? theSP.numGraves : DEFAULT_NUM_GRAVES;
+
+        this.graves = [];
+        theSP.graves.forEach((grave) => {
+            this.graves.push(new PBGrave(this.map, grave));
+        });
     }
 
     serialize(): string {
@@ -36,7 +40,16 @@ class PBPlot implements SerializablePlot {
         theJSON += '"id":' + JSON.stringify(this.id) + ', ';
         theJSON += '"location":' + JSON.stringify(this.location) + ', ';
         theJSON += '"angle":' + JSON.stringify(this.angle) + ', ';
-        theJSON += '"numGraves":' + JSON.stringify(this.numGraves);
+        theJSON += '"numGraves":' + JSON.stringify(this.numGraves) + ',';
+
+
+        theJSON += '\n      "graves":[\n';    // Open up the grave array.
+        this.graves.forEach((theGrave: PBGrave, index: number) => {
+            theJSON += theGrave.serialize();
+            theJSON += (index == (this.graves.length - 1)) ? '' : ',';  // No comma on the last of the array
+        });
+        theJSON += ']';   // Finish up the grave array.
+
         theJSON += '}';
         return(theJSON);
     }
