09f41367d39ccc3b48cdea4dfc141583e08be20c
Tim <timothykozak@yahoo.com>
2019-10-02 11:35:07 -0400

    Supports new SerializableGrave interface. Removed rotatePointAroundOrigin and rotatePolygonAboutOrigin.

--- src/PBGrave.ts
+++ src/PBGrave.ts
@@ -2,17 +2,17 @@
 //
 //
 
-import {LatLngLit, SerializableGrave} from "./PBInterfaces.js";
+import {SerializableGrave} from "./PBInterfaces.js";
+import {PBConst} from "./PBConst.js";
 
-const DEFAULT_OFFSET: LatLngLit = {lat: 0, lng: 0};
-const DEFAULT_SIZE: LatLngLit = {lat: 0, lng: 0};
+const DEFAULT_WIDTH: number = 4;
+const DEFAULT_LENGTH: number = 12;
 
 class PBGrave implements SerializableGrave {
-    offset: LatLngLit;
-    angle: number;
-    size: LatLngLit;
     name: string;   // Name of interred, or owner if not yet used
     dates: string;  // Birth and death dates
+    width: number;
+    length: number;
     validGrave: boolean;
 
     constructor(public map: google.maps.Map, theSG: SerializableGrave) {
@@ -21,11 +21,10 @@ class PBGrave implements SerializableGrave {
 
     deSerialize(theSG: SerializableGrave) {
         this.validGrave = true;
-        this.offset = !(theSG.offset == null) ? theSG.offset : DEFAULT_OFFSET;
-        this.angle = !(theSG.angle == null)  ? theSG.angle : 0;
-        this.size = !(theSG.size == null)  ? theSG.size : DEFAULT_SIZE;
         this.name = !(theSG.name == null)  ? theSG.name : '';
         this.dates = !(theSG.dates == null)  ? theSG.dates : '';
+        this.width = !(theSG.width == null) ? theSG.width : PBConst.GRAVE.width;
+        this.length = !(theSG.length == null) ? theSG.length : PBConst.GRAVE.length;
         if ((this.name.length + this.dates.length) == 0)
             this.validGrave = false;
     }
@@ -34,9 +33,8 @@ class PBGrave implements SerializableGrave {
         let theJSON = '\n' + padding + '{';
         theJSON += '"name":' + JSON.stringify(this.name) + ', ';
         theJSON += '"dates":' + JSON.stringify(this.dates) + ', ';
-        theJSON += '"offset":' + JSON.stringify(this.offset) + ', ';
-        theJSON += '"size":' + JSON.stringify(this.size) + ', ';
-        theJSON += '"angle":' + JSON.stringify(this.angle) + ' ';
+        theJSON += '"width":' + JSON.stringify(this.width) + ', ';
+        theJSON += '"length":' + JSON.stringify(this.length);
         theJSON += '}';
         return(theJSON);
     }
@@ -52,33 +50,6 @@ class PBGrave implements SerializableGrave {
         return(totalTextToSearch.toLowerCase());
     }
 
-    static rotatePointAroundOrigin(point: google.maps.Point, origin: google.maps.Point, angle: number) {
-        // The angle is in degrees.
-        // This does a simple 2D rotation of a point about an origin.
-        // Before using this, it is necessary to convert the LatLngLit to Point
-        let angleRad = angle * Math.PI / 180.0;
-        return {
-            x: Math.cos(angleRad) * (point.x - origin.x) - Math.sin(angleRad) * (point.y - origin.y) + origin.x,
-            y: Math.sin(angleRad) * (point.x - origin.x) + Math.cos(angleRad) * (point.y - origin.y) + origin.y
-        };
-    }
-
-    rotatePolygonAroundOrigin(polygon: google.maps.Polygon, orgPt: LatLngLit, theAngle: number): Array<LatLngLit> {
-        // This function returns the coordinates of the rotated Polygon.  theAngle is in degrees.
-        // The Polygons are drawn on the surface of a sphere, but rotatePointAroundOrigin is based on a 2D plane.
-        // The lat & lng of the Polygon needs to be converted to 2D by way of the map projection.
-        // Before we can get the projection, we need to wait for the map to dispatch the projection_changed event.
-        // Found this at https://stackoverflow.com/questions/26049552/google-maps-api-rotate-rectangle
-        let prj = this.map.getProjection();
-        let origin = prj.fromLatLngToPoint(new google.maps.LatLng(orgPt));
-
-        let coords = polygon.getPath().getArray().map((latLng) => {
-            let point = prj.fromLatLngToPoint(latLng);
-            let rotatedLatLng =  prj.fromPointToLatLng(PBGrave.rotatePointAroundOrigin(point, origin, theAngle) as google.maps.Point);
-            return {lat: rotatedLatLng.lat(), lng: rotatedLatLng.lng()};
-        });
-        return(coords);
-    }
 }
 
 export {PBGrave, SerializableGrave};
