dbe1c00e275d469e168c943caf771e08ee04701b
Tim <timothykozak@yahoo.com>
2019-10-02 08:43:57 -0400

    Some comments.

--- src/PBInterfaces.ts
+++ src/PBInterfaces.ts
@@ -7,12 +7,12 @@ import {PBPlot} from "./PBPlot.js";
 
 type LatLngLit = google.maps.LatLngLiteral;
 
-interface SerializableGrave {
-    offset: LatLngLit,  // Offset from primary landmark of cemetery
-    angle: number,      // Angle from principal axis of cemetery
-    size: LatLngLit,    // Size of the plot
-    name: string,       // Name of interred
-    dates: string       // Birth and death dates
+interface SerializableGrave {   // See PBGrave for descriptions of these properties.
+    offset: LatLngLit,
+    angle: number,
+    size: LatLngLit,
+    name: string,
+    dates: string
 }
 
 interface GraveInfo {
@@ -22,7 +22,7 @@ interface GraveInfo {
     theGrave: PBGrave
 }
 
-interface SerializablePlot {
+interface SerializablePlot {    // See PBPlot for descriptions of these properties.
     id: number;
     northFeet: number,
     eastFeet: number,
@@ -31,22 +31,20 @@ interface SerializablePlot {
     graves: Array<PBGrave>
 }
 
-interface SerializableCemetery {
-    location: LatLngLit,            // Location of the landmark from which all plots
-                                    // are offset in feet along the principal axis and
-                                    // its orthogonal.
-    name: string,                   //
+interface SerializableCemetery {    // See PBCemetery for descriptions of these properties.
+    location: LatLngLit,
+    name: string,
     town: string,
-    description: string,            // Shows up when hovering over the cemetery
-    boundaries: Array<LatLngLit>,   // The actual points of the cemetery boundary
-    zoom: number,                   // For zooming in to this cemetery
-    angle: number,                  // Angle of the principle axis of the cemetery,
-                                    // in degrees clockwise from north
-    graves: Array<PBGrave>,         // Graves that have not been assigned to a plot
+    description: string,
+    boundaries: Array<LatLngLit>,
+    zoom: number,
+    angle: number,
+
+    graves: Array<PBGrave>,
     plots: Array<PBPlot>
 }
 
-interface SerializableGraveFinder {
+interface SerializableGraveFinder { // See PBGraveFinder for descriptions of these properties.
     initialLatLng: google.maps.LatLng;
     cemeteries: Array<PBCemetery>;
 }
