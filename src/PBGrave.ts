6a9d767c6202ecefe375d7800365b4207d70594f
timothykozak <timothykozak@yahoo.com>
2019-10-16 17:06:05 -0400

    Now supports new SerializableGrave.

--- src/PBGrave.ts
+++ src/PBGrave.ts
@@ -2,17 +2,12 @@
 //
 //
 
-import {SerializableGrave} from "./PBInterfaces.js";
-import {PBConst} from "./PBConst.js";
-
-const DEFAULT_WIDTH: number = 4;
-const DEFAULT_LENGTH: number = 12;
+import {SerializableGrave, GraveState} from "./PBInterfaces.js";
 
 class PBGrave implements SerializableGrave {
     name: string;   // Name of interred, or owner if not yet used
     dates: string;  // Birth and death dates
-    width: number;
-    length: number;
+    state: GraveState;  // Use PBConst.GRAVE_STATE
     validGrave: boolean;
 
     constructor(public map: google.maps.Map, theSG: SerializableGrave) {
@@ -23,8 +18,7 @@ class PBGrave implements SerializableGrave {
         this.validGrave = true;
         this.name = !(theSG.name == null)  ? theSG.name : '';
         this.dates = !(theSG.dates == null)  ? theSG.dates : '';
-        this.width = !(theSG.width == null) ? theSG.width : PBConst.GRAVE.width;
-        this.length = !(theSG.length == null) ? theSG.length : PBConst.GRAVE.length;
+        this.state = !(theSG.state == null) ? theSG.state : GraveState.Interred;
         if ((this.name.length + this.dates.length) == 0)
             this.validGrave = false;
     }
@@ -33,8 +27,7 @@ class PBGrave implements SerializableGrave {
         let theJSON = '\n' + padding + '{';
         theJSON += '"name":' + JSON.stringify(this.name) + ', ';
         theJSON += '"dates":' + JSON.stringify(this.dates) + ', ';
-        theJSON += '"width":' + JSON.stringify(this.width) + ', ';
-        theJSON += '"length":' + JSON.stringify(this.length);
+        theJSON += '"state":' + JSON.stringify(this.state);
         theJSON += '}';
         return(theJSON);
     }
@@ -47,4 +40,4 @@ class PBGrave implements SerializableGrave {
 
 }
 
-export {PBGrave, SerializableGrave};
+export {PBGrave};
