80042e32a9916f86e652774adfa997a3f995a645
Tim <timothykozak@yahoo.com>
2020-02-11 21:09:36 -0500

    Initial support for AppOptions.

--- src/PBOptions.ts
+++ src/PBOptions.ts
@@ -4,11 +4,17 @@
 
 import {PBOcclusion} from "./PBOcclusion.js";
 import {PBConst} from "./PBConst.js";
+import {AppOptions} from "./PBInterfaces.js";
 
 class PBOptions extends PBOcclusion {
 
+    boundaryElement: HTMLInputElement;
+    plotsElement: HTMLInputElement;
+    gravesElement: HTMLInputElement;
+
     saveButton: HTMLButtonElement;
     exitButton: HTMLButtonElement;
+    appOptions: AppOptions = {DrawBoundary: false, DrawGraves: false, DrawPlots: false};
 
     constructor(occludedDiv: HTMLDivElement) {
         super(occludedDiv, false);  // Generate the basic occlusion HTML without the text or the OK button
@@ -29,9 +35,9 @@ class PBOptions extends PBOcclusion {
     waitForElementsToBeInstantiated() {
         // Can't get the elements until they have been instantiated.
         // Wait until the last one is instantiated.
-        let buttonElement = document.getElementById('add-grave-exit');
+        let buttonElement = document.getElementById('options-exit');
         if (buttonElement) {
-            window.dispatchEvent(new Event(PBConst.EVENTS.requestCemeteryNames));
+            this.prepareUI();
         }
         else
             setTimeout(() => {this.waitForElementsToBeInstantiated();}, 100);
@@ -40,10 +46,12 @@ class PBOptions extends PBOcclusion {
     initAddElements(): string {
         // Add all of the elements to define the grave.
         let theHTML = ` <table>
-                            <tr><td>Cemetery: </td><td><select id="add-grave-cemetery"></select></td></tr>
+                            <tr><td><input type="checkbox" id="options-boundary">Boundary</input></td></tr>
+                            <tr><td><input type="checkbox" id="options-plots">Plots</input></td></tr>
+                            <tr><td><input type="checkbox" id="options-graves">Graves</input></td></tr>
                         </table>
                         <div class="button-div">
-                            <button type="button" id="add-grave-exit" onclick="window.dispatchEvent(new Event('${PBConst.EVENTS.closeOptions}'))">Exit</button>
+                            <button type="button" id="options-exit" onclick="window.dispatchEvent(new Event('${PBConst.EVENTS.closeOptions}'))">Exit</button>
                         </div>`;
         return(theHTML);
     }
@@ -53,15 +61,24 @@ class PBOptions extends PBOcclusion {
 
     getElements() {
         // Get all of the elements
-        this.saveButton = document.getElementById('add-grave-save') as HTMLButtonElement;
-        this.exitButton = document.getElementById('add-grave-exit') as HTMLButtonElement;
+        this.boundaryElement = document.getElementById('options-boundary') as HTMLInputElement;
+        this.plotsElement = document.getElementById('options-plots') as HTMLInputElement;
+        this.gravesElement = document.getElementById('options-graves') as HTMLInputElement;
+
+        this.saveButton = document.getElementById('options-save') as HTMLButtonElement;
+        this.exitButton = document.getElementById('options-exit') as HTMLButtonElement;
     }
 
-    onSaveClick(event: Event) {
+    initOptions() {
+        this.boundaryElement.checked = this.appOptions.DrawBoundary;
+        this.plotsElement.checked = this.appOptions.DrawPlots;
+        this.gravesElement.checked = this.appOptions.DrawGraves;
     }
 
-    onExitClick(event: Event) {
-        this.deactivate();
+    prepareUI() {
+        this.getElements();
+        this.initEventListeners();
+        this.initOptions();
     }
 }
 
