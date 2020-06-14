873ee326466632af40201c71886dd0b6eb58b14f
Tim <timothykozak@yahoo.com>
2020-06-14 19:06:09 -0400

    Now supports the different grave types.

--- src/PBOptions.ts
+++ src/PBOptions.ts
@@ -11,10 +11,14 @@ class PBOptions extends PBOcclusion {
     boundaryElement: HTMLInputElement;
     plotsElement: HTMLInputElement;
     gravesElement: HTMLInputElement;
+    interredElement: HTMLInputElement;
+    reservedElement: HTMLInputElement;
+    unavailableElement: HTMLInputElement;
+    unassignedElement: HTMLInputElement;
 
     saveButton: HTMLButtonElement;
     exitButton: HTMLButtonElement;
-    appOptions: AppOptions = {DrawBoundary: false, DrawGraves: false, DrawPlots: false};
+    appOptions: AppOptions = {DrawBoundary: false, DrawPlots: false, ShowInterred: true, ShowReserved: false, ShowUnavailable: false, ShowUnassigned: false};
 
     constructor(occludedDiv: HTMLDivElement) {
         super(occludedDiv, false);  // Generate the basic occlusion HTML without the text or the OK button
@@ -46,9 +50,12 @@ class PBOptions extends PBOcclusion {
     generateHTML(): string {
         // The HTML to define the elements.
         let theHTML = ` <table>
-                            <tr><td><input type="checkbox" id="options-boundary">Boundary</input></td></tr>
-                            <tr><td><input type="checkbox" id="options-plots">Plots</input></td></tr>
-                            <tr><td><input type="checkbox" style="visibility: hidden;" id="options-graves"></input></td></tr>
+                            <tr><td><input type="checkbox" id="options-boundary">Cemetery Boundary</input></td></tr>
+                            <tr><td><input type="checkbox" id="options-plots">Cemetery Plots</input></td></tr>
+                            <tr><td><input type="checkbox" id="options-interred">Interred</input></td></tr>
+                            <tr><td><input type="checkbox" id="options-reserved">Reserved</input></td></tr>
+                            <tr><td><input type="checkbox" id="options-unavailable">Unavailable</input></td></tr>
+                            <tr><td><input type="checkbox" id="options-unassigned">Unassigned</input></td></tr>
                         </table>
                         <div class="button-div">
                             <button type="button" id="options-exit" onclick="window.dispatchEvent(new Event('${PBConst.EVENTS.closeOptions}'))">Exit</button>
@@ -63,7 +70,10 @@ class PBOptions extends PBOcclusion {
         // Get all of the elements
         this.boundaryElement = document.getElementById('options-boundary') as HTMLInputElement;
         this.plotsElement = document.getElementById('options-plots') as HTMLInputElement;
-        this.gravesElement = document.getElementById('options-graves') as HTMLInputElement;
+        this.interredElement = document.getElementById('options-interred') as HTMLInputElement;
+        this.reservedElement = document.getElementById('options-reserved') as HTMLInputElement;
+        this.unavailableElement = document.getElementById('options-unavailable') as HTMLInputElement;
+        this.unassignedElement = document.getElementById('options-unassigned') as HTMLInputElement;
 
         this.saveButton = document.getElementById('options-save') as HTMLButtonElement;
         this.exitButton = document.getElementById('options-exit') as HTMLButtonElement;
@@ -73,13 +83,19 @@ class PBOptions extends PBOcclusion {
         // Set the
         this.boundaryElement.oninput = (event) => this.onInput(event);
         this.plotsElement.oninput = (event) => this.onInput(event);
-        this.gravesElement.oninput = (event) => this.onInput(event);
+        this.interredElement.oninput = (event) => this.onInput(event);
+        this.reservedElement.oninput = (event) => this.onInput(event);
+        this.unavailableElement.oninput = (event) => this.onInput(event);
+        this.unassignedElement.oninput = (event) => this.onInput(event);
     }
 
     initOptions() {
         this.boundaryElement.checked = this.appOptions.DrawBoundary;
         this.plotsElement.checked = this.appOptions.DrawPlots;
-        this.gravesElement.checked = this.appOptions.DrawGraves;
+        this.interredElement.checked = this.appOptions.ShowInterred;
+        this.reservedElement.checked = this.appOptions.ShowReserved;
+        this.unavailableElement.checked = this.appOptions.ShowUnavailable;
+        this.unassignedElement.checked = this.appOptions.ShowUnassigned;
     }
 
     prepareUI() {
@@ -91,7 +107,10 @@ class PBOptions extends PBOcclusion {
     getOptions() {
         this.appOptions.DrawBoundary = this.boundaryElement.checked;
         this.appOptions.DrawPlots = this.plotsElement.checked;
-        this.appOptions.DrawGraves = this.gravesElement.checked;
+        this.appOptions.ShowInterred = this.interredElement.checked;
+        this.appOptions.ShowReserved = this.reservedElement.checked;
+        this.appOptions.ShowUnavailable = this.unavailableElement.checked;
+        this.appOptions.ShowUnassigned = this.unassignedElement.checked;
     }
 
     onInput(event: Event) {
